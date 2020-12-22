0d1e64ae3c09e75b6b9c4abd96b4193b4b8f78b8
SebastianMG <jsmancerag@correo.udistrital.edu.co>
2020-12-22 11:49:28 -0500

    Initial commit

---
+++ models/gasto_models.py
@@ -0,0 +1,8 @@
+from pydantic import BaseModel
+from datetime import datetime
+
+class Gasto(BaseModel):
+    username: str
+    name: str
+    date: datetime
+    value: int
---
+++ models/user_models.py
@@ -0,0 +1,14 @@
+from pydantic import  BaseModel
+from typing import List, Optional
+
+from models.gasto_models import Gasto
+
+
+class UserIn(BaseModel):
+    username: str
+    password: str
+
+class UserOut(BaseModel):
+    username: str
+    gasto: Optional[List[Gasto]] = None
+
